0be821ada08b772e7d48a46f5496d69ddbb91ebc
WaterGod <736103253@qq.com>
2023-04-27 16:18:01 +0800

    家长个人信息编辑

--- src/components/card/ParentInformationCard.vue
+++ src/components/card/ParentInformationCard.vue
@@ -3,10 +3,30 @@
     <template #header>
       <div class="card-header">
         <span>个人信息</span>
+        <el-button type="primary" text @click="handleEditButton">
+          {{ isEdit ? "提交" : "编辑" }}</el-button
+        >
+        <el-button type="info" text @click="isEdit = !isEdit" v-if="isEdit"
+          >取消</el-button
+        >
       </div>
     </template>
-    <div class="text item">姓名：{{ parentStore.parent.name }}</div>
-    <div class="text item">个人简介：{{ parentStore.parent.information }}</div>
+    <div v-if="!isEdit">
+      <div class="text item">姓名：{{ parentStore.parent.name }}</div>
+      <div class="text item">个人简介：{{ parentStore.parent.information }}</div>
+    </div>
+    <div v-else>
+      <div class="text item">
+        姓名： <el-input v-model="editParent.parentName" class="information-input" />
+      </div>
+      <div class="text item">
+        个人简介：<el-input
+          v-model="editParent.information"
+          class="information-input"
+          type="textarea"
+        />
+      </div>
+    </div>
   </el-card>
 </template>
 <script lang="ts" setup>
@@ -22,6 +42,58 @@ const parentStore = useParentStore()
 onMounted(() => {
   parentStore.refresh(axios, user.value.id)
 })
+
+const isEdit = ref(false)
+const editParent = ref()
+function handleEditButton() {
+  if (isEdit.value) {
+    if (editParent.value.parentName !== "" && editParent.value.information !== "") {
+      axios({
+        method: "put",
+        url: "/parent/upd",
+        params: editParent.value,
+      })
+        .then((res) => {
+          let data = res.data
+          if (data.code == 1) {
+            ElNotification({
+              title: "成功",
+              type: "success",
+              message: data.message,
+            })
+            isEdit.value = false
+            parentStore.refresh(axios, user.value.id)
+          } else {
+            ElNotification({
+              title: "错误",
+              type: "error",
+              message: data.message,
+            })
+          }
+        })
+        .catch((res) => {
+          ElNotification({
+            title: "错误",
+            type: "error",
+            message: "出错了！",
+          })
+        })
+    } else {
+      ElNotification({
+        title: "警告",
+        type: "warning",
+        message: "输入信息不能为空！",
+      })
+    }
+  } else {
+    isEdit.value = true
+    editParent.value = {
+      id: parentStore.parent.id,
+      parentName: parentStore.parent.name,
+      information: parentStore.parent.information,
+    }
+  }
+}
 </script>
 <style scoped>
 .card-header {
@@ -44,5 +116,8 @@ onMounted(() => {
 }
 .card-body {
   display: flex;
+}
+.information-input {
+  width: 40%;
 }
 </style>
