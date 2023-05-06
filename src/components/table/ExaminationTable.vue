01ffd31d441b599225ffea1897a0dba6e0fdb205
WaterGod <736103253@qq.com>
2023-05-06 12:00:51 +0800

    个人分数纵向对比

--- src/components/table/ExaminationTable.vue
+++ src/components/table/ExaminationTable.vue
@@ -15,7 +15,9 @@
       >
         <template #default="scope">
           <div v-if="!scope.row.isEdit">
-            {{ handleScoreText(scope.row.scores[scope.column.property]) }}
+            <el-button text bg @click="handleScoreButton(scope)">
+              {{ handleScoreText(scope.row.scores[scope.column.property]) }}</el-button
+            >
           </div>
           <div v-else>
             <el-input
@@ -69,6 +71,19 @@
     >
       <div class="person-echart-container">
         <div id="personEchart" class="person-echart"></div>
+      </div>
+    </el-dialog>
+    <el-dialog
+      title="单科纵向对比图"
+      align-center
+      v-model="scoreDialog"
+      @opened="handleScoreDialogOpen"
+      @close="handleScoreDialogClose"
+      :fullscreen="true"
+      :destroy-on-close="true"
+    >
+      <div class="person-echart-container">
+        <div id="scoreEchart" class="person-echart"></div>
       </div>
     </el-dialog>
   </div>
@@ -265,6 +280,79 @@ function handleDialogOpen() {
 }
 function handleDialogClose() {
   personEchart.dispose() //dialog关闭时清空echart实例
+}
+const scoreDialog = ref(false)
+let scoreEchartDom = null
+let scoreEchart = null
+
+// prettier-ignore
+const data = [["2000-06-05", 116], ["2000-06-06", 129], ["2000-06-07", 135], ["2000-06-08", 86], ["2000-06-09", 73], ["2000-06-10", 85], ["2000-06-11", 73], ["2000-06-12", 68], ["2000-06-13", 92], ["2000-06-14", 130], ["2000-06-15", 245], ["2000-06-16", 139], ["2000-06-17", 115], ["2000-06-18", 111], ["2000-06-19", 309], ["2000-06-20", 206], ["2000-06-21", 137], ["2000-06-22", 128], ["2000-06-23", 85], ["2000-06-24", 94], ["2000-06-25", 71], ["2000-06-26", 106], ["2000-06-27", 84], ["2000-06-28", 93], ["2000-06-29", 85], ["2000-06-30", 73], ["2000-07-01", 83], ["2000-07-02", 125], ["2000-07-03", 107], ["2000-07-04", 82], ["2000-07-05", 44], ["2000-07-06", 72], ["2000-07-07", 106], ["2000-07-08", 107], ["2000-07-09", 66], ["2000-07-10", 91], ["2000-07-11", 92], ["2000-07-12", 113], ["2000-07-13", 107], ["2000-07-14", 131], ["2000-07-15", 111], ["2000-07-16", 64], ["2000-07-17", 69], ["2000-07-18", 88], ["2000-07-19", 77], ["2000-07-20", 83], ["2000-07-21", 111], ["2000-07-22", 57], ["2000-07-23", 55], ["2000-07-24", 60]];
+const dateList = data.map(function (item) {
+  return item[0]
+})
+const valueList = data.map(function (item) {
+  return item[1]
+})
+let scoreOption = {
+  xAxis: {
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: "line",
+      label: {
+        show: true,
+        position: "left",
+        textStyle: {
+          color: "black",
+          fontsize: 16,
+        },
+        formatter: function (params) {
+          return params.value
+        },
+      },
+    },
+  ],
+}
+let scoreStudentNum
+let scoreSubjectId
+function handleScoreButton(scope) {
+  scoreDialog.value = true
+  axios({
+    method: "get",
+    url:
+      "/examination/getSubjectScores/" +
+      scope.row.studentNum +
+      "/" +
+      scope.column.property,
+  }).then((res) => {
+    let data = res.data
+    if (data.code == 1) {
+      let xData = data.data.map(function (item) {
+        return item["examinationName"]
+      })
+      let yData = data.data.map(function (item) {
+        return item["score"]
+      })
+      scoreOption.xAxis.data = xData
+      scoreOption.series[0].data = yData
+      console.log(xData)
+    }
+  })
+}
+function handleScoreDialogOpen() {
+  //图表初始化
+  scoreEchartDom = document.getElementById("scoreEchart")
+  scoreEchart = echarts.init(scoreEchartDom)
+  scoreOption && scoreEchart.setOption(scoreOption)
+}
+function handleScoreDialogClose() {
+  scoreEchart.dispose() //dialog关闭时清空echart实例
 }
 </script>
 <style scoped>
